e9cd9d646819305c7f4660e3adf4853433b3166a
joshgumerove <jgumerove@kaufmanrossin.com>
2022-04-28 12:51:33 -0400

    render api data

--- src/components/TableData.jsx
+++ src/components/TableData.jsx
@@ -13,13 +13,31 @@ const TableData = () => {
   console.log(data);
   return (
     <React.Fragment>
-      <tr>
-        <td>Fake ID</td>
+      {/* <td>Fake ID</td>
         <td>1040</td>
         <td>A very Difficult Return</td>
         <td>10</td>
-        <td>4/15/2021</td>
-      </tr>
+        <td>4/15/2021</td> */}
+      {data.map(
+        ({
+          client_id,
+          due_date,
+          return_type,
+          description,
+          time_estimate,
+          id,
+        }) => {
+          return (
+            <tr key={id}>
+              <td>{client_id}</td>
+              <td>{return_type}</td>
+              <td>{description}</td>
+              <td>{time_estimate}</td>
+              <td>{due_date}</td>
+            </tr>
+          );
+        }
+      )}
     </React.Fragment>
   );
 };
