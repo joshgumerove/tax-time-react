f0111c5e77ff63dbae6c6f3a11577c5b22a10fc3
joshgumerove <jgumerove@kaufmanrossin.com>
2022-04-29 10:53:25 -0400

    handle form inputs

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
 
 const Form = () => {
-  const [name, setName] = useState("");
+  const api = "http://localhost:3001/api/returns";
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    alert(`Submitting Name ${name}`);
+  const [clientId, setClientId] = useState(0);
+  const [returnType, setReturnType] = useState("");
+  const [description, setDescription] = useState("");
+  const [time, setTime] = useState(0);
+  const [date, setDate] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newReturn = {
+      id: parseInt(clientId),
+      return_type: returnType,
+      description: description,
+      time_estimate: parseInt(time),
+      date: new Date(date),
+    };
+    fetch(api)
+      .then((res) => res.json())
+      .then(console.log("success"));
+    console.log(newReturn);
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -13,40 +29,40 @@ const Form = () => {
         <label>Client ID:</label>
         <input
           type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={clientId}
+          onChange={(e) => setClientId(e.target.value)}
         />
       </div>
       <div>
         <label>Return Type</label>
         <input
           type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={returnType}
+          onChange={(e) => setReturnType(e.target.value)}
         />
       </div>
       <div>
         <label>Description</label>
         <textarea
           type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         />
       </div>
       <div>
         <label>Hours Estimate</label>
         <input
           type="number"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
         />
       </div>
       <div>
-        <label>Hours Estimate</label>
+        <label>Due Date</label>
         <input
           type="date"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
         />
       </div>
 
